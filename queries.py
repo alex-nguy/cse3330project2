7382965b72265f8241a052b7c5badb5e102bf935
amusealex <alexnguyen142@gmail.com>
2021-04-16 18:51:07 -0500

    Added Question 9b

--- queries.py
+++ queries.py
@@ -7,49 +7,47 @@ c = conn.cursor()
 # c.execute("SELECT * FROM CUSTOMER")
 # print(c.fetchall())
 
-# QUESTION 1
-newName = input("Enter your name: ")
-c.execute("INSERT INTO CUSTOMER(Name) VALUES(:newName)", {'newName': newName})
+# # QUESTION 1
+# newName = input("Enter your name: ")
+# c.execute("INSERT INTO CUSTOMER(Name) VALUES(:newName)", {'newName': newName})
 
-# QUESTION 2
-c.execute("UPDATE CUSTOMER SET Phone = '(837) 721-8965' WHERE Name = :newName", {'newName': newName})
+# # QUESTION 2
+# c.execute("UPDATE CUSTOMER SET Phone = '(837) 721-8965' WHERE Name = :newName", {'newName': newName})
 
-# # Checks Question 1 & 2
-# c.execute("SELECT CustID, Name, Phone FROM CUSTOMER WHERE Phone = '(837) 721-8965'")
-# print(c.fetchall())
+# # # Checks Question 1 & 2
+# # c.execute("SELECT CustID, Name, Phone FROM CUSTOMER WHERE Phone = '(837) 721-8965'")
+# # print(c.fetchall())
 
-# QUESTION 3
-c.execute("UPDATE RATE SET Daily = Daily * 1.05 WHERE Category = 1")
+# # QUESTION 3
+# c.execute("UPDATE RATE SET Daily = Daily * 1.05 WHERE Category = 1")
 
-# # Checks Question 3
-# c.execute("SELECT Daily FROM RATE WHERE Category = 1")
-# print(c.fetchall())
+# # # Checks Question 3
+# # c.execute("SELECT Daily FROM RATE WHERE Category = 1")
+# # print(c.fetchall())
 
-# QUESTION 4a
-c.execute("INSERT INTO VEHICLE VALUES('5FNRL6H58KB133711', 'Honda Odyssey', 2019, 6, 1)")
+# # QUESTION 4a
+# c.execute("INSERT INTO VEHICLE VALUES('5FNRL6H58KB133711', 'Honda Odyssey', 2019, 6, 1)")
 
-# Checks Question 4a
-# c.execute("SELECT * FROM VEHICLE WHERE VehicleID = '1GB3KZCG1EF117132'")
-# print(c.fetchall())
-# print(c.fetchall())
+# # Checks Question 4a
+# # c.execute("SELECT * FROM VEHICLE WHERE VehicleID = '1GB3KZCG1EF117132'")
+# # print(c.fetchall())
+# # print(c.fetchall())
 
-# QUESTION 4b
-c.execute("INSERT INTO RATE VALUES(5, 1, 900.00, 150.00), (6, 1, 800.00, 135.00)")
+# # QUESTION 4b
+# c.execute("INSERT INTO RATE VALUES(5, 1, 900.00, 150.00), (6, 1, 800.00, 135.00)")
 
-# Checks Question 4b
-# c.execute("SELECT * FROM RATE")
-# print(c.fetchall())
+# # Checks Question 4b
+# # c.execute("SELECT * FROM RATE")
+# # print(c.fetchall())
 
-# QUESTION 5
+# # QUESTION 5
 c.execute("SELECT VEHICLE.VehicleID, VEHICLE.Description, VEHICLE.Year FROM VEHICLE, RENTAL WHERE VEHICLE.Type = 1 AND VEHICLE.Category = 1 AND RENTAL.StartDate BETWEEN DATE('2019-06-01') AND DATE('2019-06-20')")
-print(c.fetchall())
 
-# QUESTION 6
+# # QUESTION 6
 c.execute("SELECT CUSTOMER.Name, SUM(RENTAL.TotalAmount) FROM CUSTOMER, RENTAL WHERE CUSTOMER.CustID = 221 AND RENTAL.PaymentDate IS NULL")
-print(c.fetchall())
 
 # QUESTION 7
-c.execute("
+c.execute("""
 SELECT VEHICLE.VehicleID AS VIN, 
     VEHICLE.Description, 
     VEHICLE.Year, 
@@ -69,21 +67,40 @@ SELECT VEHICLE.VehicleID AS VIN,
     RATE.Daily  
 FROM VEHICLE, RATE 
 WHERE VEHICLE.Type = RATE.Type AND VEHICLE.Category = RATE.Category
-ORDER BY Category DESC, VEHICLE.Type ASC")
-print(c.fetchall())
+ORDER BY Category DESC, VEHICLE.Type ASC""")
 
 # QUESTION 8
 c.execute("SELECT SUM(TotalAmount) FROM RENTAL WHERE PaymentDate < DATE('now')")
 print(c.fetchall())
 
 # Checks Question 9a
-c.execute("SELECT v.Description, v.Year, v.Type, v.Category, julianday(r.returndate) - julianday(r.startdate) AS TotalDuration,
-	CASE r.rentaltype
-    	WHEN 1 THEN 'Daily'
+c.execute("""
+SELECT v.Description, 
+    v.Year, 
+    CASE v.Type
+        WHEN 1 THEN 'Compact'
+        WHEN 2 THEN 'Medium'
+        WHEN 3 THEN 'Large'
+        WHEN 4 THEN 'SUV'
+        WHEN 5 THEN 'Truck'
+        WHEN 6 THEN 'VAN'
+        END Type,
+    CASE v.Category
+        WHEN 0 THEN 'Basic'
+        WHEN 1 THEN 'Luxury'
+        END Category, 
+    CASE
+        WHEN julianday(r.returndate) - julianday(r.startdate) = 1 THEN (julianday(r.returndate) - julianday(r.startdate)) || ' Day'
+        WHEN julianday(r.returndate) - julianday(r.startdate) < 7 THEN (julianday(r.returndate) - julianday(r.startdate)) || ' Days'
+        WHEN julianday(r.returndate) - julianday(r.startdate) = 7 THEN ((julianday(r.returndate) - julianday(r.startdate)) / 7) || ' Week'
+        ELSE ((julianday(r.returndate) - julianday(r.startdate)) / 7) || ' Weeks'
+        END TotalDuration,
+    CASE r.rentaltype
+        WHEN 1 THEN 'Daily'
         When 7 THEN 'Weekly'
         END RentalType,
     CASE When r.PaymentDate IS NULL
-    	THEN 'Unpaid'
+        THEN 'Unpaid'
         ELSE 'Paid'
         END PaymentStatus,
     r.TotalAmount
@@ -92,26 +109,36 @@ INNER JOIN RENTAL r
 ON r.VehicleID = v.VehicleID
 INNER JOIN CUSTOMER c
 ON r.CustID = c.CustID AND c.Name = 'J. Brown'
-ORDER By r.StartDate")
+ORDER By r.StartDate""")
+
 
 # Checks Question 9b
+c.execute("""SELECT SUM(r.TotalAmount) 
+            FROM CUSTOMER c
+            INNER JOIN RENTAL r
+            ON r.CustID = c.CustID AND c.Name = 'J. Brown'
+            WHERE r.PaymentDate IS NULL""")
+
+
 
 # Checks Question 10
-c.execute("SELECT DISTINCT	c.Name, r.StartDate, r.ReturnDate, r.TotalAmount
+c.execute("""SELECT DISTINCT	c.Name, r.StartDate, r.ReturnDate, r.TotalAmount
             FROM	RENTAL r, CUSTOMER c, VEHICLE v
-            WHERE	r.RentalType == 7 AND v.VehicleID == '19VDE1F3XEE414842' AND r.CustID == c.CustID AND r.PaymentDate IS NULL")
+            WHERE	r.RentalType == 7 AND v.VehicleID == '19VDE1F3XEE414842' AND r.CustID == c.CustID AND r.PaymentDate IS NULL""")
+
 
 # Checks Question 11
-c.execute("SELECT	c.CustID, c.Name, c.Phone
-            FROM	CUSTOMER c,
-            LEFT JOIN RENTAL ON c.CustID == RENTAL.CustID
-            WHERE	RENTAL.CustID IS NULL")
+c.execute("""SELECT	c.CustID, c.Name, c.Phone
+            FROM	CUSTOMER c
+            LEFT JOIN RENTAL ON c.CustID = RENTAL.CustID
+            WHERE	RENTAL.CustID IS NULL""")
 
 # Checks Question 12
-c.execute("SELECT DISTINCT c.Name, v.Description, r.StartDate, r.ReturnDate, r.TotalAmount
+c.execute("""SELECT DISTINCT c.Name, v.Description, r.StartDate, r.ReturnDate, r.TotalAmount
             FROM	CUSTOMER c, RENTAL r, VEHICLE v
             WHERE	c.CustID == r.CustID AND r.StartDate == r.PaymentDate AND v.VehicleID == r.VehicleID
-            ORDER BY c.Name")
+            ORDER BY c.Name""")
+
 
 conn.commit()
 conn.close()
