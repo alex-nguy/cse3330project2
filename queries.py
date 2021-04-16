a87132294195f6fe988c24e50522546074cb7931
annguyens <an.nguyen8@mavs.uta.edu>
2021-04-16 12:08:13 -0500

    q10-12

--- queries.py
+++ queries.py
@@ -31,6 +31,7 @@ c.execute("INSERT INTO VEHICLE VALUES('5FNRL6H58KB133711', 'Honda Odyssey', 2019
 # Checks Question 4a
 # c.execute("SELECT * FROM VEHICLE WHERE VehicleID = '1GB3KZCG1EF117132'")
 # print(c.fetchall())
+# print(c.fetchall())
 
 # QUESTION 4b
 c.execute("INSERT INTO RATE VALUES(5, 1, 900.00, 150.00), (6, 1, 800.00, 135.00)")
@@ -39,6 +40,31 @@ c.execute("INSERT INTO RATE VALUES(5, 1, 900.00, 150.00), (6, 1, 800.00, 135.00)
 # c.execute("SELECT * FROM RATE")
 # print(c.fetchall())
 
+# Checks Question 9a
+c.execute("SELECT DISTINCT v.Description, v.Year, v.Type, v.Category, r.TotalAmount
+    FROM VEHICLE v, RENTAL r, CUSTOMER c
+    WHERE c.CustID == 210 AND c.CustID == r.CustID AND v.VehicleID == r.VehicleID
+    ORDER BY r.StartDate")
+print(c.fetchall())
+
+# Checks Question 9b
+
+# Checks Question 10
+c.execute("SELECT DISTINCT	c.Name, r.StartDate, r.ReturnDate, r.TotalAmount
+            FROM	RENTAL r, CUSTOMER c, VEHICLE v
+            WHERE	r.RentalType == 7 AND v.VehicleID == "19VDE1F3XEE414842" AND r.CustID == c.CustID AND r.PaymentDate IS NULL")
+
+# Checks Question 11
+c.execute("SELECT	c.CustID, c.Name, c.Phone
+            FROM	CUSTOMER c,
+            LEFT JOIN RENTAL ON c.CustID == RENTAL.CustID
+            WHERE	RENTAL.CustID IS NULL")
+
+# Checks Question 12
+c.execute("SELECT DISTINCT c.Name, v.Description, r.StartDate, r.ReturnDate, r.TotalAmount
+            FROM	CUSTOMER c, RENTAL r, VEHICLE v
+            WHERE	c.CustID == r.CustID AND r.StartDate == r.PaymentDate AND v.VehicleID == r.VehicleID
+            ORDER BY c.Name")
 
 
 conn.commit()
