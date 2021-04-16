ea9b20b20511f02f7a930ef65b8e6abb9d7291d7
amusealex <alexnguyen142@gmail.com>
2021-04-15 20:00:13 -0500

    Set example

--- queries.py
+++ queries.py
@@ -3,6 +3,9 @@ import sqlite3
 conn = sqlite3.connect('CarRental.db')
 c = conn.cursor()
 
+# Prints all values in CUSTOMER Table
+# c.execute("SELECT * FROM CUSTOMER")
+# print(c.fetchall())
 
 conn.commit()
 conn.close()
