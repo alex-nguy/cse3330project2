20b47b470ac83beb3d78ceead00bcd720894a2ba
amusealex <alexnguyen142@gmail.com>
2021-04-16 19:03:41 -0500

    Final Commit

--- queries.py
+++ queries.py
@@ -3,47 +3,26 @@ import sqlite3
 conn = sqlite3.connect('CarRental.db')
 c = conn.cursor()
 
-# Prints all values in CUSTOMER Table
-# c.execute("SELECT * FROM CUSTOMER")
-# print(c.fetchall())
+# QUESTION 1
+newName = input("Enter your name: ")
+c.execute("INSERT INTO CUSTOMER(Name) VALUES(:newName)", {'newName': newName})
 
-# # QUESTION 1
-# newName = input("Enter your name: ")
-# c.execute("INSERT INTO CUSTOMER(Name) VALUES(:newName)", {'newName': newName})
+# QUESTION 2
+c.execute("UPDATE CUSTOMER SET Phone = '(837) 721-8965' WHERE Name = :newName", {'newName': newName})
 
-# # QUESTION 2
-# c.execute("UPDATE CUSTOMER SET Phone = '(837) 721-8965' WHERE Name = :newName", {'newName': newName})
+# QUESTION 3
+c.execute("UPDATE RATE SET Daily = Daily * 1.05 WHERE Category = 1")
 
-# # # Checks Question 1 & 2
-# # c.execute("SELECT CustID, Name, Phone FROM CUSTOMER WHERE Phone = '(837) 721-8965'")
-# # print(c.fetchall())
+# QUESTION 4a
+c.execute("INSERT INTO VEHICLE VALUES('5FNRL6H58KB133711', 'Honda Odyssey', 2019, 6, 1)")
 
-# # QUESTION 3
-# c.execute("UPDATE RATE SET Daily = Daily * 1.05 WHERE Category = 1")
+# QUESTION 4b
+c.execute("INSERT INTO RATE VALUES(5, 1, 900.00, 150.00), (6, 1, 800.00, 135.00)")
 
-# # # Checks Question 3
-# # c.execute("SELECT Daily FROM RATE WHERE Category = 1")
-# # print(c.fetchall())
-
-# # QUESTION 4a
-# c.execute("INSERT INTO VEHICLE VALUES('5FNRL6H58KB133711', 'Honda Odyssey', 2019, 6, 1)")
-
-# # Checks Question 4a
-# # c.execute("SELECT * FROM VEHICLE WHERE VehicleID = '1GB3KZCG1EF117132'")
-# # print(c.fetchall())
-# # print(c.fetchall())
-
-# # QUESTION 4b
-# c.execute("INSERT INTO RATE VALUES(5, 1, 900.00, 150.00), (6, 1, 800.00, 135.00)")
-
-# # Checks Question 4b
-# # c.execute("SELECT * FROM RATE")
-# # print(c.fetchall())
-
-# # QUESTION 5
+# QUESTION 5
 c.execute("SELECT VEHICLE.VehicleID, VEHICLE.Description, VEHICLE.Year FROM VEHICLE, RENTAL WHERE VEHICLE.Type = 1 AND VEHICLE.Category = 1 AND RENTAL.StartDate BETWEEN DATE('2019-06-01') AND DATE('2019-06-20')")
 
-# # QUESTION 6
+# QUESTION 6
 c.execute("SELECT CUSTOMER.Name, SUM(RENTAL.TotalAmount) FROM CUSTOMER, RENTAL WHERE CUSTOMER.CustID = 221 AND RENTAL.PaymentDate IS NULL")
 
 # QUESTION 7
@@ -71,7 +50,6 @@ ORDER BY Category DESC, VEHICLE.Type ASC""")
 
 # QUESTION 8
 c.execute("SELECT SUM(TotalAmount) FROM RENTAL WHERE PaymentDate < DATE('now')")
-print(c.fetchall())
 
 # Checks Question 9a
 c.execute("""
@@ -88,7 +66,8 @@ SELECT v.Description,
     CASE v.Category
         WHEN 0 THEN 'Basic'
         WHEN 1 THEN 'Luxury'
-        END Category, 
+        END Category,
+    r.TotalAmount / r.Qty AS UnitPrice,
     CASE
         WHEN julianday(r.returndate) - julianday(r.startdate) = 1 THEN (julianday(r.returndate) - julianday(r.startdate)) || ' Day'
         WHEN julianday(r.returndate) - julianday(r.startdate) < 7 THEN (julianday(r.returndate) - julianday(r.startdate)) || ' Days'
@@ -111,15 +90,12 @@ INNER JOIN CUSTOMER c
 ON r.CustID = c.CustID AND c.Name = 'J. Brown'
 ORDER By r.StartDate""")
 
-
 # Checks Question 9b
 c.execute("""SELECT SUM(r.TotalAmount) 
             FROM CUSTOMER c
             INNER JOIN RENTAL r
             ON r.CustID = c.CustID AND c.Name = 'J. Brown'
             WHERE r.PaymentDate IS NULL""")
-
-
 
 # Checks Question 10
 c.execute("""SELECT DISTINCT	c.Name, r.StartDate, r.ReturnDate, r.TotalAmount
